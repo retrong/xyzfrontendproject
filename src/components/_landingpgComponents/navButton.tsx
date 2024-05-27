bb8e3b600d34615b77870705f72275d3e7d2a3e8
retrong <eguobabarth@gmail.com>
2024-05-27 10:37:04 +0100

    modified styling

--- src/components/_landingpgComponents/navButton.tsx
+++ src/components/_landingpgComponents/navButton.tsx
@@ -7,7 +7,7 @@ interface NavButtonProps {
 
 export default function NavButton({children, styles} : NavButtonProps) {
   return (
-		<button
+		<button type='submit'
 			className={`rounded-2xl border-foundation-purple-purple-200 hover:border-none border leading-[32px] cursor-pointer items-center justify-center py-2 px-6 gap-[4px] ${styles}`}
 		>
 			{children}
