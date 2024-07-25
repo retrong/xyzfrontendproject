c7bfbe52aab99b8c5bace49fb32898f03a4eb662
retrong <eguobabarth@gmail.com>
2024-07-25 16:08:48 +0100

    implement business account page

---
+++ src/app/(pages)/setup/business-account/_components/companyFormList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CompanyForm from './companyForm';
+
+export default function CompanyFormList() {
+  return (
+		<>
+			<CompanyForm companyName="Gfams" />
+			<CompanyForm companyName="ASAP" />
+		</>
+	);
+}
